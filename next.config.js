8a5a4a2f5d33ba50b355085b71dfaf5eff12f4c3
Forgingalex <olaiyaalexander97@gmail.com>
2026-05-17 19:04:42 +0100

    chore: migrate project to Lumina Protocol and update dependencies

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracingRoot: __dirname,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
   },
 }
 
 module.exports = nextConfig
-
 
